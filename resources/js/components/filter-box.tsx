ad41f6b3d5b0b052266f6cfd6775279aae455275
ErinAbduli <erinabduli7@gmail.com>
2026-01-09 22:08:04 +0100

    continued the filter box

--- resources/js/components/filter-box.tsx
+++ resources/js/components/filter-box.tsx
@@ -2,6 +2,9 @@ const FilterBox = () => {
     return (
         <div className="flex h-[500px] w-[200px] flex-col rounded-md border border-[#373737] p-4">
             <h3>Filter</h3>
+            <div>
+                <div></div>
+            </div>
         </div>
     );
 };
